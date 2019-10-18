e32566974ab9f60aa174c10e8a2908c9e9529202
kunlingou <kunlingou@foxmail.com>
2019-10-18 14:10:52 +0800

    init

---
+++ taotao-manager/taotao-manager-dao/src/test/java/LeanCloudTest.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
+import cn.leancloud.AVObject;
+import cn.leancloud.AVQuery;
+import cn.leancloud.core.AVOSCloud;
+import cn.leancloud.query.AVCloudQueryResult;
+import io.reactivex.Observable;
+
+public class LeanCloudTest {
+	
+	public static void main(String[] args) {
+		AVOSCloud.initialize("EVsneirELmgOUBYlT40edXpt-gzGzoHsz", "n16mFPPYxAOdKRoIPef3tUSV");
+//		AVObject avObject = new AVObject("Test");
+//		avObject.put("name", "goukunlin");
+//		avObject.saveInBackground().blockingSubscribe();
+		Observable<AVCloudQueryResult> doCloudQueryInBackground = AVQuery.doCloudQueryInBackground("select * from Counter");
+		AVCloudQueryResult blockingFirst = doCloudQueryInBackground.blockingFirst();
+		List<AVObject> find = (List<AVObject>) blockingFirst.getResults();
+//		AVQuery<AVObject> query = AVQuery.getQuery("Counter");
+//		List<AVObject> find = query.find();
+		System.out.println(Arrays.deepToString(find.toArray()));
+	}
+}
